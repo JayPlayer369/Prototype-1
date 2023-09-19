257d2f59e92b18717160324cd3028abb3fc1581d
Ahacker1208 <97680603+Ahacker1208@users.noreply.github.com>
2023-09-19 12:39:15 -0400

    Finished 1.3

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{   public float speed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //moves the vehicle forward
+        transform.Translate(0, 0, 1);
+        //Time.deltaTime helps to keep track the speed and time between all of the different frames
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
